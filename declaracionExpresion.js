cb9239a6fe8f1daecb6571b480d4fef83fe44fbc
Alfredo Supanta <alfredo.supanta.26@gmail.com>
2022-07-22 22:38:36 -0400

    funciones declaradas y expresadas, variables locales y variables globales, alcance, arrow function

--- declaracionExpresion.js
+++ declaracionExpresion.js
@@ -2,12 +2,18 @@
 
 saludar();
 
+//declaracion de funcion, esta puede ser llamada antes de ser declarada
 function saludar (){
     console.log("hola ")
 }
 
 buenosDias;//por el hoisting no se puede llamar a esta funcion antes de ser declarada
 
+
+//Expresion de funcion, esta no se puede llamar antes de ser declarada
+    //se pueden usar arrow function o funciones anonimas
+
 let buenosDias = () => {//por ser expresada no se puede llamar antes
     console.log("Buenos días")
 }
+
---
+++ scopeGlobalLocal.js
@@ -0,0 +1,55 @@
+// variable Local
+  //se usan en el cuerpo principal
+  //se usan en funciones
+  //una funcion solo debe operar con informacion de alcance local
+  //lo que necesite del exterior debe comunicarse por argumentos
+  //lo que necesite comunicar al exterior debe ser por retorno
+
+let nombre = "alfredo";
+
+function saludar(){
+  let edad = 29;
+  console.log(nombre,edad);// alfredo 29
+}
+saludar();
+
+console.log(nombre); //alfredo
+//console.log(edad); // error, edad is not defined (esta dentro de una variable local)
+
+
+//2) ejemplo
+
+
+function presentacion(nombre, admin){
+  if(admin){
+    rol = "administrador";// porque funciona si no esta definido el tipo de variable
+  }else{
+    rol = "usuario"
+  }
+  console.log("soy " + nombre + " y mi rol es: " + rol);
+}
+
+//presentacion("alfredo", 0);//0 js lo considera false
+presentacion("alfredo", true);//0 js lo considera false
+
+//-------------------------------------------------------------------------------------------------
+
+//Variable Global
+  //solo en la funcion declarada
+
+
+
+//-------------------------------------------------------------------------------------------------
+
+//Arrow function
+  // sirve cuando queremos llamar propiedades de un scope local
+
+let obj = {
+  numeros: [1,2,3],
+  imprimir: function(){
+    this.numeros.forEach(()=>{
+      console.log(this);
+    })
+  }
+}
+obj.imprimir();
